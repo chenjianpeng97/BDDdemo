500f371bfa0bd6278206e8486ef16fb77442f590
chenj <chenjianpeg97@outlook.com>
2024-09-18 23:28:38 +0800

    初始化项目

---
+++ test/unit/environment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: Tuner
+@contact:chenjianpeng97@outlook.com
+@version: 1.0.0
+@license: Apache Licence
+@file: environment.py
+@time: 2024/9/2 23:01
+"""
+if __name__ == '__main__':
+    pass
