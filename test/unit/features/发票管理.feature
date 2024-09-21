5ebf40f0e0f9434087480687e3fede2f1f5b71d2
chenj <chenjianpeg97@outlook.com>
2024-09-21 15:39:34 +0800

    完善调整财务余额方法的步骤定义

--- test/unit/features/发票管理.feature
+++
@@ -1,6 +0,0 @@
-# Created by chenj at 2024/9/4
-Feature: # Enter feature name here
-  # Enter feature description here
-
-  Scenario: # Enter scenario name here
-    # Enter steps here
--- test/unit/steps/step.py
+++ test/unit/steps/step.py
@@ -9,73 +9,96 @@
 @file: step.py
 @time: 2024/9/3 0:35
 """
-from behave import given, when, then
+from behave import given, when, then, use_step_matcher
 
 
 class User:
     def __init__(self, name, role):
-        self.name = name
-        self.role = role
+        self.name = name  # 用户名
+        self.role = role  # 用户角色
+
+    def login(self, name):
+        if name == "self.name":
+            return {"token": self.role}
+        else:
+            return "没有此用户"
 
 
-# 假设有一个账户管理的类
+# 假设有一个账户类
 class Account:
     def __init__(self, cus_name, balance):
-        self.balance = balance
-        self.cus_name = cus_name
+        self.balance = balance  # 账户余额
+        self.cus_name = cus_name  # 经销商名称
 
     # 打印account
     def __str__(self):
         return f"{self.cus_name}的账户"
 
-    def adjust_balance(self, account, operation, value, operate_user):
-        if operate_user.role != "财务":
-            return "系统报错信息显示“权限不足”"
+
+# -- 以上可能是系统已有的代码 --#
+
+# 现在实现这调整功能加入账户管理类
+class AccountManager:
+    def __init__(self, dealer_account: Account, operator: User):
+        self.account = dealer_account
+        self.operator = operator
+
+    def adjust_balance(self, operation, change_value) -> str:
         if operation == "增加":
-            self.balance += value
-            return f"{account}余额变为{self.balance}"
-        elif operation == "减少":
-            if value > self.balance:
-                return "系统报错信息显示“账户余额最多减少至0”"
-            self.balance -= value
-            return f"{account}余额变为{self.balance}"
-
-    def put_change_record(self):
-        # 增加余额调整记录的方法
-        pass
+            self.account.balance += change_value
+        # 减少时最多减少到0
+        elif operation == "减少" and self.account.balance - change_value >= 0:
+            self.account.balance -= change_value
+        else:
+            return "账户余额最多减少至0"
+        return "操作成功"
 
 
-# 初始化实例
-user = User(name="钱经理", role="财务")
-account = Account(cus_name="健康医疗", balance=500)
+use_step_matcher("parse")
 
 
-@given('以财务角色"{user}"登录系统')
-def step_impl(context, user):
-    context.user = user
+@given('以"{role}"角色"{user}"登录系统')
+def step_impl(context, role, user):
+    # 初始化实例
+    # 这里是单元测试，我们测试实现这个调整功能的类，故UnitTest层级中的测试直接实例用户对象就可以了
+    context.user = User(name=user, role=role)
 
 
 @given('系统中已有一个账户"{account_name}"')
 def step_impl(context, account_name):
     # 这里可以根据需要进行扩展
-    assert account_name == context.account.cus_name
+    pass
 
 
 @given('"{account_name}"账户余额为"{balance}"元')
 def step_impl(context, account_name, balance):
-    assert context.account.balance == int(balance)
+    context.account = Account(cus_name=account_name, balance=int(balance))
 
 
-@when('"{role}"对"{account_name}"的账户做{operation}并输入{change_value}')
-def step_impl(context, role, account_name, operation, change_value):
-    context.result = context.account_manager.adjust_balance(
-        account_name, operation, int(change_value)
+@when('"{user}"对"{account_name}"的账户做{operation}并输入{change_value}')
+def step_impl(context, user, account_name, operation, change_value):
+    change_value = int(change_value)
+    account_manager = AccountManager(
+        dealer_account=context.account, operator=context.user
     )
+    context.result = account_manager.adjust_balance(operation, change_value)
 
 
-@then('本次操作{result}')
-def step_impl(context, result):
-    assert context.result == result
+use_step_matcher("re")
+
+
+@then(r'.*余额变为(\d+)')
+def step_impl(context, excpected_balance):
+    assert context.account.balance == int(excpected_balance)
+
+
+@then(r'.*显示"(\w+)"')
+def step_impl(context, warning_message):
+    assert context.result == warning_message, \
+        f"实际报错信息：{context.result}，期望报错信息：{warning_message}"
+
+
+use_step_matcher("parse")
 
 
 @when('进入经销商账户变动记录页面')
